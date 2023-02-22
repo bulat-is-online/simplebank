c5b80305292295039926a12b1ced7c625b08214d
bulat-is-online <125478741+bulat-is-online@users.noreply.github.com>
2023-02-22 22:22:51 +0300

    Add files via upload

---
+++ db/query/transfer.sql
@@ -0,0 +1,36 @@
+CREATE TABLE "transfers" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
+  "ammount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+-- name: CreateTransfer :one
+INSERT INTO transfers (
+  from_account_id,
+  to_account_id,
+  amount
+) VALUES (
+  $1, $2, $3
+)
+RETURNING *;
+
+-- name: GetTransfer :one
+SELECT * FROM transfers
+WHERE id = $1 LIMIT 1;
+
+-- name: ListTransfers :many
+SELECT * FROM transfers
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateTransfer :exec
+UPDATE transfers
+SET amount = $2
+WHERE id = $1;
+
+-- name: DeleteTransfer :exec
+DELETE FROM transfers
+WHERE id = $1;
