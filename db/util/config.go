7e0719513d5a395ae57f05b02cf6ebf07731d520
bulat-is-online <bulat.is.online@gmail.com>
2023-04-28 16:23:47 +0300

    viper - config reader

---
+++ db/util/config.go
@@ -0,0 +1,28 @@
+package util
+
+import "github.com/spf13/viper"
+
+// Contains all configurations of the app
+// Values are read by Viper
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// Reads or rewrites config values
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
--- main.go
+++ main.go
@@ -6,19 +6,17 @@ import (
 
 	"github.com/bulat-is-online/simplebank/api"
 	db "github.com/bulat-is-online/simplebank/db/sqlc"
+	"github.com/bulat-is-online/simplebank/db/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	var err error
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot read config")
+	}
 
-	conn, err := sql.Open(dbDriver, dbSource)
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -26,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("can't start server:", err)
 	}
