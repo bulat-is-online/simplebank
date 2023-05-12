f3009f010ac1b14ff396a764bc109c1024f48cc7
bulat-is-online <bulat.is.online@gmail.com>
2023-05-12 11:04:30 +0300

    fixed bug

--- api/user.go
+++ api/user.go
@@ -61,8 +61,8 @@ func (server *Server) createUser(ctx *gin.Context) {
 
 	rsp := createUserResponse{
 		Username:          user.Username,
-		FullName:          user.Username,
-		Email:             user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
 		PasswordChangedAt: user.PasswordChangedAt,
 		CreatedAt:         user.CreatedAt,
 	}
